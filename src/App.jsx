e44e59dce0ba6b4e6f60145fb11990c234b74ddb
Leonardo Recalde <leo03recalde@gmail.com>
2024-08-29 16:20:16 -0300

    inicio nuevo proyectp

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  
+  return (
+    <>
+    <main>
+      <h1 className='text-center'>Bienvenidos</h1>
+      
+    </main>
+    </>
+    
+  )
+}
+
+export default App
