50d7a671ba2c4d27a68376173a9044d2943f4ebf
Leonardo Recalde <leo03recalde@gmail.com>
2024-09-12 14:09:35 -0300

    agrago readme

--- src/components/ListaTareas.jsx
+++ src/components/ListaTareas.jsx
@@ -1,15 +1,18 @@
 import ListGroup from "react-bootstrap/ListGroup";
 import ItemTarea from "./ItemTarea";
 
-const ListaTareas = ({arrayTareas, borrarTarea}) => {
+const ListaTareas = ({ arrayTareas, borrarTarea }) => {
   return (
     <div>
       <ListGroup>
-        {
-            arrayTareas.map((tarea, indice)=> <ItemTarea key={indice} tarea={tarea} borrarTarea={borrarTarea}></ItemTarea>)
-        }
+        {arrayTareas.map((tarea, indice) => (
+          <ItemTarea
+            key={indice}
+            tarea={tarea}
+            borrarTarea={borrarTarea}
+          ></ItemTarea>
+        ))}
       </ListGroup>
-      
     </div>
   );
 };
